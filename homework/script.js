4ecb5e7c26b1e9f1be7d50f022e2d8f7ac8e373b
Kalista Setiawan <setiawankalista501@gmail.com>
2021-07-09 14:41:37 +0700

    Update data

--- homework/script.js
+++ homework/script.js
@@ -1,9 +1,18 @@
-  $('#submit').click(function () {
-    let title = $('#title').val();
-    let desc = $('#desc').val();
-    alert("Good job! The title you entered = "+title+" and the description = " +desc);
-    $('#judul').append(title);
-    document.getElementById('title').value = "";
-    document.getElementById('desc').value = "";
-    });
-  });
+ function getValue() {
+      alert("Welcome to track list!");
+
+      axios.get('https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json')
+      .then(function (response) {
+        let title = response.data.name;
+        let artist = response.data.artists[0].name;
+        let album = response.data.album.name;
+
+        document.getElementById('title').append(title);
+        document.getElementById('artist').append(artist);
+        document.getElementById('album').append(album);
+      })
+      .catch(function (e) {
+        console.log(e)
+      });
+    }
+    
