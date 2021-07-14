11ad2306d279eb8d8169d3820f2fd3f0b968dd9b
Kalista Setiawan <setiawankalista501@gmail.com>
2021-07-14 13:12:19 +0700

    update style

--- generasi-gigih-homework/src/App.js
+++ generasi-gigih-homework/src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import New from './pages/Home/New';
 import './App.css';
 
 function App() {
 
-  const axios = require("axios")
-
-    // const {
-    //   REACT_APP_CLIENT_ID,
-    //   REACT_APP_REDIRECT_URL
-    // } = process.env;
-
-  axios.get(`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&scope=user-read-private%20user-read-email&state=34fFs29kd09`)
-  .then(function (response) {
-    console.log(response)
-  })
-  .catch(function (e) {
-    console.log(e)
-  });
-
   return (
     <div className="App">
-      <header className="App-header">
+      
+      {/* <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -34,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
